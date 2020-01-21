08520cfd1f27e6d77190ffe8313d0a441c5b8928
Anderain123 <anderuri@gmail.com>
2020-01-21 17:25:37 +0100

    a

--- Pedido.java
+++ Pedido.java
@@ -23,15 +23,44 @@ public class Pedido
        nombreCliente = nombre;
        carmelo = 1;
     }
-    
+    /**
+     * Devuelve la direccion de envio.
+     * 
+     * @param No hay que introducir nada.
+     * @return devuelve una String con la direccion de envio.
+     */
     public String getDireccion()
     {
         return direccionEnvio;
     }
-    
+    /**
+     * Devuelve el nombre del cliente.
+     * 
+     * @param No hay que introducir nada
+     * @return Devuelve una String con el nombre del cliente.
+     */
     public String getNombreCliente()
     {
         return nombreCliente;
     }
-    
+    /**
+     * Cambia la direccion de envia por una nueva.
+     * 
+     * @param La nueva direccion de envio(String).
+     * @return No devuelve nada.
+     */
+    public void setDireccion(String nuevaDireccion)
+    {
+        direccionEnvio = nuevaDireccion;
+    }
+    /**
+     * Cambia el nombre del cliente por uno nuevo.
+     * 
+     * @param El nuevo nombre del cliente(String).
+     * @return No devuelve nada.
+     */
+    public void setNombreCliente(String nuevoNombreCliente)
+    {
+        nombreCliente = nuevoNombreCliente;
+    }
 }
